4d4530ecec2eca10e52399cf6bbf4072f8517fc7
kkk <kkk@163.com>
2026-08-14 18:43:32 +0800

    add priority and cancel

--- threadpool.cpp
+++ threadpool.cpp
@@ -7,7 +7,20 @@
 #include<condition_variable>
 #include<string>
 #include<chrono>
+#include<unordered_map>
 using namespace std;
+struct TaskNode {
+    int priority;
+    uint64_t id;
+    function<void()> func;
+
+    // priority_queue 默认是最大堆，用 > 实现最小堆（数值越小越先执行）
+    bool operator<(const TaskNode& other) const {
+        return priority > other.priority;
+    }
+};
+
+
 class threadpool {
 public://缺public
     threadpool(int workernum) {
@@ -26,10 +39,22 @@ public://缺public
                             });
                         if (stop && tasks.empty())return;
                         //move可以避免昂贵的拷贝
-                        function<void()>task(move(tasks.front()));
+                        //tasks.top()返回的是const TaskNode&，需要去const
+                        TaskNode task=move(const_cast<TaskNode&>(tasks.top()));
                         tasks.pop();
                         lock.unlock();
-                        task();
+                        bool cancelled = false;
+                        {
+                            std::lock_guard<std::mutex> lock(cancel_mtx_);
+                            auto it = cancelmap.find(task.id);
+                            if (it != cancelmap.end()) {
+                                cancelled = it->second;
+                                cancelmap.erase(it); 
+                            } 
+                        }if (cancelled) {
+                                continue; 
+                            }
+                        task.func();
                     }
                 }
             );
@@ -38,11 +63,16 @@ public://缺public
 
     }
     template<class F, class ...ARG>
-    void addtask(F&& f, ARG && ...arg) {
+    void addtask(int priority, F&& f, ARG && ...arg) {
+        uint64_t id = ++next_id_;
+        {unique_lock<mutex>canslock(cancel_mtx_);
+        cancelmap[id] = false;
+        }
+        
         //别写成forward<ARG>((arg)...)
         function<void()>task = bind(forward<F>(f), forward<ARG>(arg)...);
         unique_lock<mutex>lock(mtx);
-        tasks.emplace(move(task));
+        tasks.emplace(TaskNode{ priority, id, move(task)});
         lock.unlock();
         condition.notify_one();
 
@@ -63,10 +93,23 @@ public://缺public
 
 
     }
+    bool cancel(uint64_t id) {
+        std::lock_guard<std::mutex> lock(cancel_mtx_);
+        auto it = cancelmap.find(id);
+        if (it != cancelmap.end()) {
+            it->second = true;   // 标记为取消
+            return true;
+        }
+        return false;
+    }
 private:
+    // 成员变量
+    priority_queue<TaskNode> tasks;
+    unordered_map<uint64_t, bool> cancelmap;  // 只存 ID -> 是否取消
+    mutex cancel_mtx_;  // 保护 cancel_map_
+    uint64_t next_id_ = 0;
     vector<thread>workers;
     //缺()
-    queue<function<void()>>tasks;
     condition_variable condition;
     mutex mtx;
     bool stop;
@@ -75,14 +118,14 @@ private:
 //忘加;
 int main() {
     threadpool pool(6);
-    for (int i = 0; i < 10; i++)
+    for (int i = 1; i <= 10; i++)
     {
-        pool.addtask([i] {
+        pool.addtask(i%2,[i] {
             printf("task %d is ok\n", i);
             this_thread::sleep_for(chrono::seconds(4));
             printf("task %d is done\n", i);
             });
     }
-    
+    pool.cancel(3);
     return 0;
 }
